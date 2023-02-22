831bca50f6360d2263ae7bb6cd419a398a5b68e9
hannahpietersen <hannahpietersen@gmail.com>
2023-02-22 16:17:15 +0530

    added code to respective files
    added html tags and unordered lists and rendered it to show on screen

---
+++ app.js
@@ -0,0 +1,15 @@
+const page = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun Facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walker</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is mainitained by Facebook</li>
+            <li>Powers through thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
