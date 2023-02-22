0329f88a6bbb2cff2fbb50296edd66107a0c41ba
hannahpietersen <hannahpietersen@gmail.com>
2023-02-22 17:30:56 +0530

    added code to respective files
    added respective components to make it look effecient and changed styling in css

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const page = (
-    <div>
+
+
+function Header(){
+   return( <header>
+    <nav>
         <img src="./react-logo.png" width="40px" />
+    </nav>
+</header>
+   )
+}
+
+function Footer(){
+   return (
+    <footer>
+       <small>© 2023 Hannah development. All rights reserved.</small>
+    </footer>
+   )
+}
+
+function MainContent(){
+    return(
+        <div>
+       <Header />
         <h1>Fun Facts about React</h1>
         <ul>
             <li>Was first released in 2013</li>
@@ -9,7 +29,10 @@ const page = (
             <li>Is mainitained by Facebook</li>
             <li>Powers through thousands of enterprise apps, including mobile apps</li>
         </ul>
+        <Footer />
     </div>
-)
+    )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+
+ReactDOM.render(<MainContent />, document.getElementById("root"))
